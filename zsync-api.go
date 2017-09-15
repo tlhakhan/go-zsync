4d15f52ae0d5283e045e28418fcd1dd082e2f775
Tenzin Lhakhang <tenzin.lhakhang@gmail.com>
2017-09-15 09:12:56 -0400

    create zsync api

---
+++ zsync-api.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"flag"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func main() {
+	var port = flag.Int("p", 8089, "api listen port")
+	flag.Parse()
+
+	r := mux.NewRouter()
+	zpoolRouter := r.PathPrefix("/api/zpool").Subrouter()
+	zfsRouter := r.PathPrefix("/api/zfs").Subrouter()
+	zpoolRouter.Methods("GET").Path("/list").HandlerFunc(zpoolHandler)
+	zfsRouter.Methods("GET").Path("/{poolName}/list").HandlerFunc(zfsHandler)
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), r))
+}
+
+func zpoolHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Zpool Test"))
+}
+
+func zfsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Zfs Test"))
+}
